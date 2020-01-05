d41289453d9ffc902c80d25af773a5e25c3a6f0e
micha <micha@192.168.0.185>
2020-01-05 04:29:47 +0100

    mnjn

---
+++ hello/src/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+}
